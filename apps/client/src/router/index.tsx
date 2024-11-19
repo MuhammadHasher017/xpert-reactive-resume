a07d4b2707c4b2188c41497cb4f5129e49e4256b
MH08 <muhammadhasher017@gmail.com>
2024-11-19 10:00:17 +0500

    delete some more files

--- apps/client/src/router/index.tsx
+++ apps/client/src/router/index.tsx
@@ -11,7 +11,6 @@ import { VerifyOtpPage } from "../pages/auth/verify-otp/page";
 import { BuilderLayout } from "../pages/builder/layout";
 import { builderLoader, BuilderPage } from "../pages/builder/page";
 import { DashboardLayout } from "../pages/dashboard/layout";
-import { ResumesPage } from "../pages/dashboard/resumes/page";
 import { SettingsPage } from "../pages/dashboard/settings/page";
 import { HomeLayout } from "../pages/home/layout";
 import { PrivacyPolicyPage } from "../pages/home/meta/privacy-policy/page";
@@ -67,7 +66,6 @@ export const routes = createRoutesFromElements(
     <Route path="dashboard">
       {/* <Route element={<AuthGuard />}> */}
       <Route element={<DashboardLayout />}>
-        <Route path="resumes" element={<ResumesPage />} />
         <Route path="settings" element={<SettingsPage />} />
 
         <Route index element={<Navigate replace to="/dashboard/resumes" />} />
